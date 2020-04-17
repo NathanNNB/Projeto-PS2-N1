9eede7861345eb9305efe2a5021ea1cd5400092c
Matheus Portilho <61274419+Helped64@users.noreply.github.com>
2020-04-17 20:43:50 -0300

    Commit 1
    Inicialização do projeto no IJ, ja adicionado dependencia para o mysql

---
+++ src/main/java/mack/br/projeton1/App.java
@@ -0,0 +1,13 @@
+package mack.br.projeton1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
